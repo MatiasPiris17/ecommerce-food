2016f1f6046fa921e3b8fb3a361a81dbd0ea2683
MatiasPiris17 <matiaslinkedin17@gmail.com>
2023-03-28 17:43:30 -0300

    modificando estilos

--- client/src/components/detail/detail.jsx
+++ client/src/components/detail/detail.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getDetail } from "../../redux/actions";
+import style from "./Detail.module.css";
 
 export default function RecipeDetail() {
   const dispatch = useDispatch();
@@ -13,35 +14,44 @@ export default function RecipeDetail() {
   }, [dispatch, id]);
 
   return (
-    <div>
-      <div>
-        <img src={detail.image} alt={detail.name} />
-      </div>
-      <h1>{detail.name}</h1>
-      <div>
-        <h2>HealthScore: {detail.healthScore}</h2>
-        <h3>Diets: {detail.diets}</h3>
-        {detail.summary && (
-          <div>
-            <h3>Summary: </h3>
-            <p dangerouslySetInnerHTML={{ __html: detail.summary }}></p>
-          </div>
-        )}
-      </div>
-      <div>
-        <p>
-          Steps:{" "}
-          {detail.steps?.map((e) => (
-            <ol>{e}</ol>
-          ))}
-        </p>
-        <div>
-          <Link to="/home">
-            <button>
-              <b>HOME</b>
-            </button>
-          </Link>
+    <div className={style.containerDetail}>
+      <div className={style.recipeDetail}>
+        <h1 className={style.recipeTitle}>{detail.name}</h1>
+        <div className={style.recipeImgContainer}>
+          <img
+            className={style.recipeImg}
+            src={detail.image}
+            alt={detail.name}
+          />
         </div>
+        <div className={style.recipeData}>
+          <h4 className={style.recipeHealthScore}>
+            Health Score: {detail.healthScore}
+          </h4>
+          <h4 className={style.recipeDiets}>Diets: {detail.diets}</h4>
+          {detail.summary && (
+            <div className={style.recipeSummary}>
+              <h3 className={style.recipeSummaryTitle}>Summary:</h3>
+              <p
+                className={style.recipeSummaryContent}
+                dangerouslySetInnerHTML={{ __html: detail.summary }}
+              ></p>
+            </div>
+          )}
+        </div>
+        <div className={style.recipeSteps}>
+          <p className={style.recipeStepsTitle}>Steps:</p>
+          <ol className={style.recipeStepsContent}>
+            {detail.steps?.map((step) => (
+              <li>{step}</li>
+            ))}
+          </ol>
+        </div>
+      </div>
+      <div className={style.back}>
+        <Link to="/home">
+          <button className={style.btn}>HOME</button>
+        </Link>
       </div>
     </div>
   );
